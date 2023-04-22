1bcc82407772f8ba60dc116d4afb3021b72d6e2a
jameskl237 <djielejames@gmail.com>
2023-04-23 00:54:04 +0200

    nouveau projet laravel

---
+++ app/Models/Ue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ue extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'code',
+        'nom',
+        'type',
+        'libelle',
+
+    ];
+}
